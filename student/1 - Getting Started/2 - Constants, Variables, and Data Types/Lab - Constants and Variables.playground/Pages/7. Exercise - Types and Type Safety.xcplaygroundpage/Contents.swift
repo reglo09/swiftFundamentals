7fb143573ec7cf4444bfde092fc7459e76404300
reglo09 <olgereduardo09@gmail.com>
2024-08-01 20:39:56 -0600

    Update

--- student/1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
+++ student/1 - Getting Started/2 - Constants, Variables, and Data Types/Lab - Constants and Variables.playground/Pages/7. Exercise - Types and Type Safety.xcplaygroundpage/Contents.swift
@@ -3,16 +3,23 @@
  
  Declare two variables, one called `firstDecimal` and one called `secondDecimal`. Both should have decimal values. Look at both of their types by holding Option and clicking the variable name.
  */
-
+var firsDecimal = 5.5
+var secondDecimal = 10.7
 
 //:  Declare a variable called `trueOrFalse` and give it a boolean value. Try to assign it to `firstDecimal` like so: `firstDecimal = trueOrFalse`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var trueOrFalse = false
+//firsDecimal = trueOrFalse
+print("No compile because firsDecimal it's a decimal type and trueOrFalse it's a boolean")
 
 //:  Declare a variable and give it a string value. Then try to assign it to `firstDecimal`. Does it compile? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var text = "test"
+//text = firsDecimal
+print("No compile because firsDecimal it's a decimal type and text it's a string")
 
 //:  Finally, declare a variable with a whole number value. Then try to assign it to `firstDecimal`. Why won't this compile even though both variables are numbers? Print a statement to the console explaining why not, and remove the line of code that will not compile.
-
+var intNumber = 10
+intNumber = firsDecimal
+print("No compile because firsDecimal it's a decimal type and text it's a int")
 
 /*:
 [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Tracking Different Types](@next)
