f81598c30a25c1a41ee442dab0ea37c9491d5065
reglo09 <olgereduardo09@gmail.com>
2024-08-03 14:21:21 -0600

    update

--- student/1 - Getting Started/3 - Operators/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
+++ student/1 - Getting Started/3 - Operators/Lab - Operators.playground/Pages/3. Exercise - Compound Assignment.xcplaygroundpage/Contents.swift
@@ -3,7 +3,10 @@
  
  Declare a variable whose value begins at 10. Using addition, update the value to 15 using the compound assignment operator. Using multiplication, update the value to 30 using compound assignment. Print out the variable's value after each assignment.
  */
-
+var number = 10
+number += 5
+number *= 2
+print(number)
 
 /*:
  Create a variable called `pennyBank` that begins at 0. You will use this to keep track of money you earn and spend. For each point below, use the right compound assignment operator to update the balance in your penny bank.
